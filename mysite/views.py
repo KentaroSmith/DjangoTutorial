b33e79a40c2609c019cfdb85e839b74d9a840dff
Daniel Smith <daniel.smith8795@gmail.com>
2020-01-17 16:04:13 -0600

    improving views and beinning to work on voting system

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
 
 def index(request):
     return HttpResponse("This is the homepage!")
